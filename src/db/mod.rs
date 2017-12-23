4084a15f9c5f8aa4aa1875a4b57758a289f52e06
Erin <erin@hashbang.sh>
2017-12-23 10:02:22 -0600

    db: add db::Connection Rocket request guard

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -1,9 +1,17 @@
+use std::ops::Deref;
 use r2d2;
 use r2d2_diesel::ConnectionManager;
 use diesel::pg::PgConnection;
+use rocket::request::{self, FromRequest};
+use rocket::{Request, State, Outcome};
+use rocket::http::Status;
 
 /// Convenient type alias for the postgres database pool so we don't have to type this out.
 type Pool = r2d2::Pool<ConnectionManager<PgConnection>>;
+
+/// Type alias for the pooled connection.
+type PooledConnection = r2d2::PooledConnection<ConnectionManager<PgConnection>>;
+
 
 /// Initializes a new connection pool for the database at `url`.
 pub fn init_connection_pool<S>(url: S) -> Result<Pool, r2d2::Error>
@@ -13,4 +21,38 @@ pub fn init_connection_pool<S>(url: S) -> Result<Pool, r2d2::Error>
 
     r2d2::Pool::builder()
         .build(manager)
+}
+
+/// Request guard type for handing out db connections from the pool.
+pub struct Connection(pub PooledConnection);
+
+
+/// Custom guard implementation so routes can grab a database connection easily.
+///
+/// Attempts to retrieve a single connection from the database pool;
+/// if no pool is online, fails with `InternalServerError`.
+/// If no connections are available, fails with `ServiceUnavailable`.
+impl <'a, 'r> FromRequest<'a, 'r> for Connection {
+    type Error = ();
+
+    fn from_request(request: &'a Request<'r>) -> request::Outcome<Connection, ()> {
+        // retrieve the database connection from Rocket's managed data
+        let pool = request.guard::<State<Pool>>()?;
+
+        match pool.get() { // .get() gets a connection from the pool
+            Ok(conn) => Outcome::Success(Connection(conn)),
+            Err(_) => Outcome::Failure((Status::ServiceUnavailable, ()))
+        }
+    }
+}
+
+/// A convenient way to use a `&db::Connection` as a `&PgConnection`.
+///
+/// Just allows deref-ing the inner PooledConnection.
+impl Deref for Connection {
+    type Target = PgConnection;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
 }
