084df73470b6b00a872f4868f5c27038b4c25795
Erin <erin@hashbang.sh>
2018-01-05 18:36:40 -0600

    implement user profile pages with templating

--- src/routes/ui.rs
+++ src/routes/ui.rs
@@ -1,17 +1,27 @@
 use diesel::prelude::*;
 use rocket::Route;
+use rocket_contrib::Template;
 use ::db;
-use db::models::User;
+use db::models::{User, Account};
 
 pub fn routes() -> Vec<Route> {
     routes![index, user_page]
 }
 
 #[get("/users/<username>", format="text/html")]
-pub fn user_page(username: String, db_conn: db::Connection) -> Option<String> {
-    let user = try_opt!(User::by_username(&db_conn, username));
+pub fn user_page(username: String, db_conn: db::Connection) -> Option<Template> {
+    let account = try_opt!(Account::fetch_local_by_username(&db_conn, username));
 
-    Some(format!("{:?}", user))
+    // We can use a cute hack to remove the need to explicitly write out a context struct,
+    // by using the serde_json helper to construct a `Serialize`-able struct on the fly.
+    let context = json!({
+        "username": &account.username,
+        "display_name": account.display_name.as_ref().unwrap_or(&account.username),
+        "uri": &account.get_uri(),
+        "bio": account.summary.as_ref().map(String::as_str).unwrap_or("<p></p>"),
+    });
+
+    Some(Template::render("user_profile", context))
 }
 
 #[get("/")]
