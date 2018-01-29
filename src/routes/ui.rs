8a7a5f0461ef31fb912ede862cca8c8fb30a8300
Erin <erin@hashbang.sh>
2018-01-29 14:29:30 -0600

    ui::routes: reimplement /users/<user> using Maud

--- src/routes/ui.rs
+++ src/routes/ui.rs
@@ -1,5 +1,5 @@
 use rocket::Route;
-use maud::{html, Markup};
+use maud::{html, Markup, PreEscaped};
 
 use db;
 use db::models::Account;
@@ -11,10 +11,34 @@ pub fn routes() -> Vec<Route> {
 }
 
 #[get("/users/<username>", format = "text/html")]
-pub fn user_page(username: String, db_conn: db::Connection) -> Perhaps<()> {
+pub fn user_page(username: String, db_conn: db::Connection) -> Perhaps<Page> {
     let account = try_resopt!(Account::fetch_local_by_username(&db_conn, username));
 
-    Ok(Some(()))
+    let rendered = PageBuilder::default()
+        .title(format!("@{user}", user = account.username))
+        .content(html! {
+            div.h-card {
+                header {
+                    h1 a.u-url.u-uid href=(account.get_uri()) {
+                        span.p-name (account.display_name.as_ref().unwrap_or(&account.username))
+                    }
+
+                    div (account.fully_qualified_username())
+                }
+
+                div.p-note {
+                    @if let Some(bio) = account.summary.as_ref() {
+                        (PreEscaped(bio))
+                    } @else {
+                        p {}
+                    }
+                }
+            }
+        })
+        .build()
+        .unwrap();
+
+    Ok(Some(rendered))
 }
 
 #[get("/")]
