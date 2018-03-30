0aadd02e6064d83256a3997fb16e83506ee819b7
Erin <erin@hashbang.sh>
2018-03-30 16:11:39 -0500

    activitypub: federate Statuses!

--- src/routes/ap.rs
+++ src/routes/ap.rs
@@ -5,13 +5,18 @@ use rocket::http::ContentType;
 use rocket_contrib::Json;
 
 use db;
-use db::models::Account;
+use db::models::{Account, Status};
 use error::Perhaps;
 use activitypub::{ActivityGuard, ActivityStreams, AsActivityPub};
 use {BASE_URL, DOMAIN};
 
 pub fn routes() -> Vec<Route> {
-    routes![ap_user_object, webfinger_get_resource, webfinger_host_meta]
+    routes![
+        ap_user_object,
+        ap_status_object,
+        webfinger_get_resource,
+        webfinger_host_meta
+    ]
 }
 
 /// Returns a user as an ActivityPub object.
@@ -24,6 +29,24 @@ pub fn ap_user_object(
     let account = try_resopt!(Account::fetch_local_by_username(&db_conn, username));
 
     Ok(Some(account.as_activitypub(&db_conn)?))
+}
+
+/// Returns a user status as an ActivityPub object.
+#[get("/users/<username>/statuses/<status_id>", rank = 2)]
+pub fn ap_status_object(
+    username: String,
+    status_id: u64,
+    _ag: ActivityGuard,
+    db_conn: db::Connection,
+) -> Perhaps<ActivityStreams> {
+    let account = try_resopt!(Account::fetch_local_by_username(&db_conn, username));
+    let status = try_resopt!(Status::by_account_and_id(
+        &db_conn,
+        account.id,
+        status_id as i64
+    ));
+
+    Ok(Some(status.as_activitypub(&db_conn)?))
 }
 
 /// A type representing the parameters of a WebFinger query.
