6a0ebfc598179ed5872cb82dac1e8833dd466bee
Erin <erin@hashbang.sh>
2018-05-02 16:40:40 -0500

    ci: fix build line :cold_sweat:

--- scripts/after_success.sh
+++ scripts/after_success.sh
@@ -1,5 +1,4 @@
 if [[ "$TRAVIS_BRANCH" == "master" ]]; then
-	cargo build --release; else cargo build
 	zip -r0 latest target/release/rustodon static/ migrations/ scripts/ appspec.yml
 	mkdir -p buildartifacts; mv latest.zip buildartifacts/latest.zip
 fi
