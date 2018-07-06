52d4339b15ef54aa39c407fe5ab623cf93bbb3ab
chris <netshade@gmail.com>
2018-07-05 23:01:56 -0400

    remove unnecessary comments

--- migrations/2018-07-06-010240_ensure-unique-user-index/down.sql
+++ migrations/2018-07-06-010240_ensure-unique-user-index/down.sql
@@ -1,3 +1,2 @@
--- This file should undo anything in `up.sql`
 UPDATE accounts SET domain = NULL WHERE domain = '';
 DROP INDEX accounts_must_have_unique_uris;
--- migrations/2018-07-06-010240_ensure-unique-user-index/up.sql
+++ migrations/2018-07-06-010240_ensure-unique-user-index/up.sql
@@ -1,3 +1,2 @@
--- Your SQL goes here
 UPDATE accounts SET domain = '' WHERE domain IS NULL;
 CREATE UNIQUE INDEX accounts_must_have_unique_uris ON accounts(uri);
