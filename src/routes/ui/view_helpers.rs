5b98b34f963bcbbe702624eb06dba2097867ddbd
Erin <erin@hashbang.sh>
2018-07-20 17:04:05 -0500

    view_helpers: remove unnecessary lifetime annotations

--- src/routes/ui/view_helpers.rs
+++ src/routes/ui/view_helpers.rs
@@ -4,11 +4,11 @@ use failure::Error;
 use transform;
 
 pub trait HasBio {
-    fn transformed_bio<'a>(&'a self, connection: &db::Connection) -> Option<String>;
+    fn transformed_bio(&self, connection: &db::Connection) -> Option<String>;
 }
 
 impl HasBio for Account {
-    fn transformed_bio<'a>(&'a self, connection: &db::Connection) -> Option<String> {
+    fn transformed_bio(&self, connection: &db::Connection) -> Option<String> {
         if let Some(raw_bio) = self.summary.as_ref().map(String::as_str) {
             transform::bio(raw_bio, |username, domain| {
                 Account::fetch_by_username_domain(connection, username, domain).map_err(Error::from)
