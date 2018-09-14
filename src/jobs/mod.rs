38991795f5d0a3f497b423d05e54d8c0bba4b93f
Erin <erin@hashbang.sh>
2018-09-14 17:39:46 -0500

    :fire: get rid of some stale code in ::jobs

--- src/jobs/mod.rs
+++ src/jobs/mod.rs
@@ -1,12 +1,7 @@
 //! Asynchronous jobs.
 
-use bincode;
+use serde_json;
 use turnstile::Job;
-
-#[derive(Serialize, Deserialize, Debug)]
-enum Jobs {
-    TestJob(TestJob),
-}
 
 #[derive(Serialize, Deserialize, Debug)]
 struct TestJob {
@@ -18,21 +13,5 @@ impl Job<()> for TestJob {
         println!("self.some_data: {:?}", self.some_data);
 
         Ok(())
-    }
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[test]
-    fn foo() {
-        let j = TestJob { some_data: 413 };
-        println!("{:?}", j);
-        let bytes = bincode::serialize(&Jobs::TestJob(j)).unwrap();
-        println!("{:?}", bytes);
-
-        let j_: Jobs = bincode::deserialize(&bytes).unwrap();
-        println!("{:?}", j_);
     }
 }
