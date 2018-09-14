f138228311a33c007ba92d561a108c65f49390f6
Erin <erin@hashbang.sh>
2018-09-14 17:44:41 -0500

    turnstile: some dtypes related to job execution contracts

--- lib/turnstile/src/lib.rs
+++ lib/turnstile/src/lib.rs
@@ -1,3 +1,20 @@
+use std::time::Duration;
+
 pub trait Job<E> {
     fn run(&self) -> Result<(), E>;
+}
+
+enum Backoff {
+    ConstantWait(Duration),
+    Exponential { base: Duration },
+}
+
+enum FailBehavior {
+    Retry(Backoff),
+    Destroy,
+}
+
+struct ExecutionContract {
+    timeout: Option<Duration>,
+    fail_behavior: FailBehavior,
 }
