bc7d1836abdbc7cf3c10c63366213fe241e3e7e5
Brayden Banks <me@bb010g.com>
2018-10-10 00:03:26 -0700

    [WIP] Rewrite of entity parsing to nom
    Entity parsing is now done in one pass, instead of multiple passes. This
    should be more efficient and guarantees that entities will not overlap.
    
    WIP notes: The current code was naïvely ported from regexes, meaning
    that characters are consumed and not avaiable for stuff like last
    character checks. These can be worked around (hashtag parsing is the
    only working parser right now and can be seen in action from the cli
    example), but I wanted to get something pushed.
    
    * Make URL parsing work (backtracking fixes)
    * Make mention parsing work
    * Remove dead regex module
    * Make tests compile
    * Make tests pass
    * Rewrite usage points in rustodon

--- lib/posticle/src/lib.rs
+++ lib/posticle/src/lib.rs
@@ -1,104 +1,98 @@
 #![feature(nll)]
-extern crate regex;
-#[macro_use]
-extern crate lazy_static;
 
 #[macro_use]
+extern crate nom;
+extern crate unic_ucd_category;
+
 #[cfg(test)]
 #[macro_use]
 extern crate pretty_assertions;
 
-mod regexes;
+use nom::IResult;
 
-#[derive(Debug, PartialEq, Eq, Hash)]
-/// Tags a given [Entity]'s semantic kind.
-pub enum EntityKind {
-    /// A URL.
-    Url,
-    /// A hashtag.
-    Hashtag,
-    /// A mention; the inner data is `(username, optional domain)`.
-    Mention(String, Option<String>),
-}
+mod parsers;
 
-#[derive(Debug, PartialEq, Eq, Hash)]
 /// Represents an entity extracted from a given string of text.
 ///
-/// The entity is described by its `kind` and the `range` of indices it occupies within the string.
-pub struct Entity {
-    pub kind:  EntityKind,
-    pub range: (usize, usize),
+/// The entity is described by its semantic kind and the `data` it comprises within a string.
+#[derive(Debug, PartialEq, Eq, Hash)]
+pub enum Entity<'a> {
+    /// A URL.
+    Url(&'a str),
+    /// A hashtag.
+    Hashtag(&'a str),
+    /// A mention; the inner data is `(username, optional domain)`.
+    Mention(&'a str, Option<&'a str>),
 }
 
-impl Entity {
-    /// Extracts the range described by this Entity from the passed `text`.
-    pub fn substr<'a>(&self, text: &'a str) -> &'a str {
-        &text[self.range.0..self.range.1]
-    }
-
-    /// Returns `true` if this entity overlaps with some `other` entity.
-    pub fn overlaps_with(&self, other: &Entity) -> bool {
-        self.range.0 <= other.range.1 && other.range.0 <= self.range.1
-    }
+/// Represents a mix of entities and text.
+#[derive(Debug, PartialEq, Eq, Hash)]
+pub enum SemanticText<'a> {
+    /// Normal text.
+    Text(&'a str),
+    /// An entity.
+    Entity(Entity<'a>),
 }
 
 /// Given `text`, extract all [Entities](Entity)
-pub fn entities(text: &str) -> Vec<Entity> {
-    if text.is_empty() {
-        return Vec::new();
-    }
-
-    let mut results = extract_urls(text);
-    results.extend(extract_hashtags(text, &results));
-    results.extend(extract_mentions(text, &results));
-
-    results.sort_by(|a, b| a.range.cmp(&b.range));
-    results
-}
-
-/// Given `text`, extract all [URL](EntityKind::Url) entities.
-pub fn extract_urls(text: &str) -> Vec<Entity> {
-    regexes::RE_URL
-        .find_iter(text)
-        .map(|mat| Entity {
-            kind:  EntityKind::Url,
-            range: (mat.start(), mat.end()),
-        }).collect()
-}
-
-/// Given `text` and some `existing` entities, extract all [Hashtag](EntityKind::Hashtag) entities
-/// which do not overlap with the `existing` ones.
-pub fn extract_hashtags(text: &str, existing: &[Entity]) -> Vec<Entity> {
-    regexes::RE_HASHTAG
-        .find_iter(text)
-        .map(|mat| Entity {
-            kind:  EntityKind::Hashtag,
-            range: (mat.start(), mat.end()),
-        }).filter(|en| {
-            existing
-                .iter()
-                .all(|existing_en| !en.overlaps_with(existing_en))
-        }).collect()
-}
-
-/// Given `text` and some `existing` entities, extract all [Mention](EntityKind::Mention) entities
-/// which do not overlap with the `existing` ones.
-pub fn extract_mentions(text: &str, existing: &[Entity]) -> Vec<Entity> {
-    regexes::RE_MENTION
-        .captures_iter(text)
-        .map(|capt| {
-            let whole = capt.get(0).unwrap();
-            let user = capt[1].to_string();
-            let domain = capt.get(2).map(|s| s.as_str().to_string());
-            Entity {
-                kind:  EntityKind::Mention(user, domain),
-                range: (whole.start(), whole.end()),
+fn entities_p<'a>(text: &'a str) -> IResult<&'a str, Vec<SemanticText<'a>>> {
+    let stext: IResult<&'a str, Vec<SemanticText<'a>>>;
+    {
+        use nom::AtEof;
+        let mut res = Vec::new();
+        let mut input = text.clone();
+        let mut text_chars = 0;
+        loop {
+            let (text_input, input_) = input.split_at(text_chars);
+            match alt_complete!(
+                input_,
+                map!(parsers::valid_url, Entity::Url)
+                    | map!(parsers::hashtag, Entity::Hashtag)
+                    | map!(parsers::mention, |(u, d)| Entity::Mention(u, d))
+            ) {
+                Ok((i, o)) => {
+                    // loop trip must always consume (otherwise infinite loops)
+                    if i == input {
+                        if i.at_eof() {
+                            stext = Ok((input, res));
+                        } else {
+                            stext = Err(nom::Err::Error(error_position!(
+                                input,
+                                nom::ErrorKind::Many0
+                            )));
+                        }
+                        break;
+                    }
+                    if text_chars > 0 {
+                        res.push(SemanticText::Text(text_input));
+                        text_chars = 0;
+                    }
+                    res.push(SemanticText::Entity(o));
+                    input = i;
+                },
+                Err(nom::Err::Error(_)) => {
+                    if input_.is_empty() {
+                        if !text_input.is_empty() || !res.is_empty() {
+                            res.push(SemanticText::Text(text_input));
+                        }
+                        stext = Ok((text_input, res));
+                        break;
+                    } else {
+                        text_chars += 1;
+                    }
+                },
+                Err(e) => {
+                    stext = Err(e);
+                    break;
+                },
             }
-        }).filter(|en| {
-            existing
-                .iter()
-                .all(|existing_en| !en.overlaps_with(existing_en))
-        }).collect()
+        }
+    }
+    stext
+}
+
+pub fn entities<'a>(text: &'a str) -> Option<Vec<SemanticText<'a>>> {
+    entities_p(text).map(|r| r.1).ok()
 }
 
 #[cfg(test)]
@@ -119,14 +113,14 @@ mod tests {
         assert_eq!(
             entities("@mention"),
             vec![Entity {
-                kind:  EntityKind::Mention("mention".to_string(), None),
+                kind:  EntityKind::Mention("mention", None),
                 range: (0, 8),
             }]
         );
         assert_eq!(
             entities("@mention@domain.place"),
             vec![Entity {
-                kind:  EntityKind::Mention("mention".to_string(), Some("domain.place".to_string())),
+                kind:  EntityKind::Mention("mention", Some("domain.place")),
                 range: (0, 21),
             }]
         );
@@ -168,7 +162,7 @@ mod tests {
                     range: (9, 28),
                 },
                 Entity {
-                    kind:  EntityKind::Mention("mention".to_string(), None),
+                    kind:  EntityKind::Mention("mention", None),
                     range: (29, 37),
                 },
             ]
