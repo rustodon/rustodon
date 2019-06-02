caa29186322078cc9166f83e6861ecd0567777e8
y6nH <y6nH@live.co.uk>
2019-06-02 11:20:13 +0100

    Set up Gulp to minify our (minimal) JavaScript too

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,21 @@
-const {series, src, dest} = require("gulp");
+const {parallel, src, dest} = require("gulp");
 const cleanCss = require("gulp-clean-css");
 const sass = require("gulp-sass");
+const terser = require("gulp-terser");
+const rename = require("gulp-rename");
 
-function defaultTask(){
-    return src("style/style.scss", { sourcemaps: true })
+async function css(){
+    return await src("style/style.scss", { sourcemaps: true })
     .pipe(sass())
     .pipe(cleanCss())
-    .pipe(dest("static", { sourcemaps: "." }))
+    .pipe(dest("static", { sourcemaps: "." }));
 }
 
-exports.default = series(defaultTask);
+async function js(){
+    return await src("static/js/accessibility.js", { sourcemaps: true })
+    .pipe(terser())
+    .pipe(rename({ extname: '.min.js' }))
+    .pipe(dest("static/js", { sourcemaps: "." }));
+}
+
+exports.default = parallel(css, js);
