65278939dba86879cb5601012eae2fa80c4f5ecb
Hugh Spiller <hs135083@renishaw.com>
2019-06-12 18:24:29 +0100

    Add SVG minimizer to Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const {parallel, src, dest} = require("gulp");
 const cleanCss = require("gulp-clean-css");
 const sass = require("gulp-sass");
 const terser = require("gulp-terser");
+const svgmin = require('gulp-svgmin');
 const rename = require("gulp-rename");
 
 async function css(){
@@ -9,13 +10,26 @@ async function css(){
     .pipe(sass())
     .pipe(cleanCss())
     .pipe(dest("static", { sourcemaps: "." }));
-}
+};
 
 async function js(){
     return await src("static/js/accessibility.js", { sourcemaps: true })
     .pipe(terser())
     .pipe(rename({ extname: '.min.js' }))
     .pipe(dest("static/js", { sourcemaps: "." }));
-}
+};
 
-exports.default = parallel(css, js);
+async function svg(){
+    return await src('static/img/icons.svg')
+    .pipe(svgmin({
+        plugins: [{
+            cleanupNumericValues: {
+                floatPrecision: 1
+            }
+        }]
+    }))
+    .pipe(rename({ extname: '.min.svg' }))
+    .pipe(dest('static/img'));
+};
+
+exports.default = parallel(css, js, svg);
