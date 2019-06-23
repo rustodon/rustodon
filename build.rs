333f063cfacd73723bc1158c851fe2b96ee36888
Hugh <y6nH@users.noreply.github.com>
2019-06-24 00:03:32 +0100

    Correct formatting in build.rs
    Thanks, Travis

--- build.rs
+++ build.rs
@@ -19,7 +19,7 @@ where
 }
 
 fn main() {
-     let out_dir = PathBuf::from(env::var_os("OUT_DIR").unwrap());
+    let out_dir = PathBuf::from(env::var_os("OUT_DIR").unwrap());
 
     File::create(out_dir.join("commit-info.txt"))
         .unwrap()
