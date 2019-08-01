dd37a9c89130024cfa64eb6a14e3ad4a27021fd5
Hugh Spiller <hs135083@renishaw.com>
2019-08-01 13:23:36 +0100

    Get Travis pushing back to GitHub (Also significant auto formatting on .travis.yml)

--- scripts/after_success.sh
+++ scripts/after_success.sh
@@ -2,3 +2,12 @@ if [[ "$TRAVIS_BRANCH" == "master" ]]; then
 	zip -r0 latest target/release/rustodon target/release/rustodonctl static/ migrations/ scripts/ appspec.yml
 	mkdir -p buildartifacts; mv latest.zip buildartifacts/latest.zip
 fi
+
+# Check in the results of the Gulp compilation
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
+git checkout -b travis-ran-gulp
+git add static
+git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+git remote add origin https://${GH_TOKEN}@github.com/rustodon/rustodon.git > /dev/null 2>&1
+git push --quiet --set-upstream origin travis-ran-gulp
